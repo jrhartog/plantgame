91ae6d488708566b48bd3e6b54f7746bc72720ab
Renate Hartog <jrhartog@gmail.com>
2017-05-02 16:39:35 -0700

    fix variable name

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,7 @@
 var menuState = {
     create : function () {
       this.button = game.add.button(game.world.centerX - 95, 400, 'seedling', this.start, this, 2, 1, 0);
-      this.button2 = game.add.button(game.world.centerX - 95, 300, 'SeedlingBrown', this.start, this, 2, 1, 0);
+      this.button2 = game.add.button(game.world.centerX - 95, 300, 'seedlingBrown', this.start, this, 2, 1, 0);
        },
     start : function () {
         game.state.start('play');
