d37cdfdb984a62c22c0634c5156261bf29d7e1c1
Renate Hartog <jrhartog@gmail.com>
2017-05-02 19:25:22 -0700

    middle function to pass correct sprite-name to start

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,14 @@
 var menuState = {
     create : function () {
-      this.button = game.add.button(game.world.centerX - 95, 400, 'seedling', this.start('seedling'), this, 0,0,0,0);
-      this.button2 = game.add.button(game.world.centerX - 95, 300, 'seedlingBrown', this.start('seedlingBrown'), this, 0,0,0,0);
+      this.button = game.add.button(game.world.centerX - 95, 400, 'seedling', this.setSprite1, this, 0,0,0,0);
+      this.button2 = game.add.button(game.world.centerX - 95, 300, 'seedlingBrown', this.setSprite2, this, 0,0,0,0);
        },
+    setSprite1 : function () {
+        this.start('seedling');
+    },
+    setSprite2 : function () {
+        this.start('seedlingBrown');
+    },
     start : function (spriteKey) {
         console.log('spriteKey: ' + spriteKey);
         game.state.start('play',false,false,spriteKey);
