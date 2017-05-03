75883cff2fe77511e21691686c54becc6a079709
Renate Hartog <jrhartog@gmail.com>
2017-05-02 19:16:13 -0700

    some logging

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,10 @@
 var menuState = {
     create : function () {
-      this.button = game.add.button(game.world.centerX - 95, 400, 'seedling', this.start('seedling'), this, 2, 1, 0);
-      this.button2 = game.add.button(game.world.centerX - 95, 300, 'seedlingBrown', this.start('seedlingBrown'), this, 2, 1, 0);
+      this.button = game.add.button(game.world.centerX - 95, 400, 'seedling', this.start('seedling'), this, 0,0,0,0);
+      this.button2 = game.add.button(game.world.centerX - 95, 300, 'seedlingBrown', this.start('seedlingBrown'), this, 0,0,0,0);
        },
     start : function (spriteKey) {
+        console.log('spriteKey: ' + spriteKey);
         game.state.start('play',false,false,spriteKey);
        }
 };
